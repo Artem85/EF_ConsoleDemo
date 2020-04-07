e697235875568a89f19a9fd057721fa9d9af9671
Zainchkovskyy Artem <zainchkovskyy.artem@gmail.com>
2020-04-08 01:49:19 +0300

    Initial commit

---
+++ DemoConsole/Entities/ActorDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoConsole.Entities
+{
+    class ActorDbContext : DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ActorDb;Trusted_Connection=True");
+        }
+    }
+}
---
+++ DemoConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using DemoConsole.Entities;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ActorDbContext())
+            {
+                db.Actors.AddRange(
+                    new Actor
+                    {
+                        Name = "Bruce Lee",
+                        Age = 75,
+                        AcademyWinner = false
+                    },
+                    new Actor
+                    {
+                        Name = "Madonna",
+                        Age = 65,
+                        AcademyWinner = true
+                    });
+
+                var count = db.SaveChanges();
+
+                Console.WriteLine($"{count} records added");
+
+                foreach (var Actor in db.Actors)
+                {
+                    Console.WriteLine($"Name: {Actor.Name},\t\t" +
+                                        $"Age: {Actor.Age},\t\t" +
+                                        $"AcademyWinner: {Actor.AcademyWinner}");
+                }
+            }
+        }
+    }
+}
